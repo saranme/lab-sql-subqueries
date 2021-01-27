6e607cd8cf81f278f7e24d5627d0c48e0c248887
saranm <s.navarrome@gmail.com>
2021-01-27 19:08:58 +0100

    update exercise

--- lab-sql-subqueries.sql
+++ lab-sql-subqueries.sql
@@ -14,6 +14,11 @@ Instructions
 */
 Use sakila;
 -- 1 How many copies of the film Hunchback Impossible exist in the inventory system?
+SELECT film_id, COUNT(film_id) n_movies
+FROM inventory i
+GROUP BY 1
+HAVING film_id = (SELECT film_id FROM film WHERE title = 'HUNCHBACK IMPOSSIBLE');
+--
 SELECT f.film_id, COUNT(f.title) n_movies, f.title
 FROM film f
 JOIN inventory i
@@ -39,6 +44,12 @@ WHERE f.film_id = (SELECT film_id
 
 -- 4 Sales have been lagging among young families, and you wish to target all family movies for a promotion. 
 -- Identify all movies categorized as family films.
+SELECT title
+FROM film
+WHERE film_id IN (SELECT film_id
+				FROM film_category
+				WHERE category_id = (SELECT category_id FROM category WHERE name = 'Family'));
+--
 SELECT fc.film_id, f.title
 FROM film_category fc
 JOIN film f
@@ -50,6 +61,15 @@ WHERE c.name = 'Family';
 -- 5 Get name and email from customers from Canada using subqueries. Do the same with joins. 
 -- Note that to create a join, you will have to identify the correct tables with their primary keys and foreign keys, 
 -- that will help you get the relevant information.
+SELECT CONCAT(first_name,' ',last_name) customer, email
+FROM customer 
+WHERE address_id IN (SELECT address_id FROM address WHERE city_id IN (SELECT DISTINCT city_id 
+												FROM city 
+                                                WHERE country_id = (SELECT country_id 
+																	FROM country 
+                                                                    WHERE country = 'Canada'))
+					);
+--
 SELECT co.country country, CONCAT(c.first_name,' ',c.last_name) customer_name, c.email
 FROM address a
 JOIN customer c
@@ -58,17 +78,7 @@ JOIN city ci
 ON a.city_id = ci.city_id
 JOIN country co
 ON co.country_id = ci.country_id
-WHERE co.country = (SELECT DISTINCT country FROM country WHERE country = 'Canada')
-
-SELECT co.country country, CONCAT(c.first_name,' ',c.last_name) customer_name, c.email
-FROM address a
-JOIN customer c
-ON a.address_id = c.customer_id
-JOIN city ci
-ON a.city_id = ci.city_id
-JOIN country co
-ON co.country_id = ci.country_id
-WHERE co.country = 'Canada';
+WHERE co.country = (SELECT DISTINCT country FROM country WHERE country = 'Canada');
 
 -- 6 Which are films starred by the most prolific actor? Most prolific actor is defined as the actor 
 -- that has acted in the most number of films. First you will have to find the most prolific actor and 
@@ -87,19 +97,18 @@ WHERE actor_id = (SELECT actor_id FROM (
 
 
 -- 7. Films rented by most profitable customer. 
--- You can use the customer table and payment table to find the most profitable customer ie the customer that has made the largest sum of payments.
-SELECT c.customer_id, CONCAT(c.first_name,' ', c.last_name) customer_name, SUM(p.amount) total_amount
-FROM customer C
-JOIN payment p
-ON c.customer_id = p.customer_id
-GROUP BY 1,2
-ORDER BY 3 DESC
-LIMIT 1;
+-- You can use the customer table and payment table 
+-- to find the most profitable customer ie the customer that has made the largest sum of payments.
+SELECT title FROM film WHERE film_id IN (
+SELECT film_id FROM inventory WHERE inventory_id IN (SELECT inventory_id FROM rental WHERE customer_id = (SELECT customer_id FROM (SELECT COUNT(rental_id) total_movies_rented, customer_id
+													FROM payment
+													GROUP BY customer_id
+													ORDER BY 1 DESC
+													LIMIT 1) AS rentals_cust
+                                                    )));
 
 -- 8. Customers who spent more than the average payments.
-SELECT c.customer_id, CONCAT(c.first_name,' ', c.last_name) customer_name, AVG(p.amount) total_amount
-FROM customer C
-JOIN payment p
-ON c.customer_id = p.customer_id
-GROUP BY 1,2
-HAVING AVG(p.amount) > (SELECT AVG(amount) FROM payment)
+SELECT SUM(amount) total_amount, customer_id
+FROM payment
+GROUP BY 2
+HAVING SUM(amount) > (SELECT AVG(amount) FROM payment);
